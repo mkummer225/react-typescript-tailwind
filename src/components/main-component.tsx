6540cdf645bd49f248dd70099de2c9572f3f791d
Matt Kummer <7843299+mkummer225@users.noreply.github.com>
2025-06-07 11:12:13 -0600

    replit config

--- src/components/main-component.tsx
+++ src/components/main-component.tsx
@@ -2,9 +2,7 @@ interface MainCompProps {
 }
 
 export default function MainComponent({ }: MainCompProps) {
-
-
-    return (<div className="relative flex-1 grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-1">
-        Vite + React + TS + Tailwind – Hello World
+    return (<div className="relative text-white p-4 flex-1 grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-1">
+        <h1 className="text-center">Vite + React + TS + Tailwind – Hello World</h1>
     </div>)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: ["localhost", "127.0.0.1", "0.0.0.0", ".replit.dev"],
+  },
 })
