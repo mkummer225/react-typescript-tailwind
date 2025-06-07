67b267757a7626b05380b5217f1ba1c11099b13b
Matt Kummer <7843299+mkummer225@users.noreply.github.com>
2025-06-07 11:14:29 -0600

    remove grid

--- src/components/main-component.tsx
+++ src/components/main-component.tsx
@@ -2,7 +2,7 @@ interface MainCompProps {
 }
 
 export default function MainComponent({ }: MainCompProps) {
-    return (<div className="relative text-white p-4 flex-1 grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-1">
+    return (<div className="relative text-white p-4 flex-1">
         <h1 className="text-center">Vite + React + TS + Tailwind – Hello World</h1>
     </div>)
 }
